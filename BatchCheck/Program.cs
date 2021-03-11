ed4b068dd8a5915aad546bfe4e23024c5084b36d
Georg Bege <georg@bege.email>
2021-03-11 23:50:20 +0100

    Added BatchCheck

---
+++ BatchCheck/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BatchCheck
+{
+    class Program
+    {
+        static private char[] trimmedChars = {'^', '\r', '\n', ' '};
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try {
+                var input = args[0];
+                var lines = File.ReadAllLines(input).SelectMany(xs => xs.Split(new char[] {'^'}, StringSplitOptions.RemoveEmptyEntries));
+                var filtered = lines.Where(xs => xs.Trim(trimmedChars).StartsWith("-"));
+                var result = String.Join("", filtered.ToArray());
+
+                // - output
+                Clipboard.SetText(result);
+                Console.WriteLine(result);
+            }
+            catch(Exception e) {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
